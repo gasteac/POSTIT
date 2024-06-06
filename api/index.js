e3094634d13a7303f8243dfda97981a3a3069fc9
Gaston Acosta <gasteac@gmail.com>
2024-06-06 03:01:06 -0300

    Remove unused import in api/index.js

--- api/index.js
+++ api/index.js
@@ -16,7 +16,6 @@ import cookieParser from "cookie-parser";
 import path from "path";
 //esto va a setear las variables del .env en process.env osea en el entorno
 dotenv.config(); 
-
 // import cors from "cors";
 // importamos las rutas de user, auth, post y comment
 import userRoutes from "./routes/user.routes.js";
